87f6e7b3ee4606d798c896a1730fef47d4a63014
khadka-baniya <133153407+khadka-123@users.noreply.github.com>
2025-04-21 00:00:40 +0530

    Update ElectionDashboard.jsx

--- src/pages/Election-Analysis/ElectionDashboard.jsx
+++ src/pages/Election-Analysis/ElectionDashboard.jsx
@@ -50,7 +50,7 @@ const ElectionDashboard = ({ party }) => {
       try {
         
         const response = await axios.get(
-          `https://server-drab-five.vercel.app/api/election_analysis/${party}`
+          `https://server-drab-five.vercel.app/election_analysis/${party}`
         );
         console.log(response.data);
         // Expecting the backend to return { success: true, data: { election_analysis: [ ... ] } }
